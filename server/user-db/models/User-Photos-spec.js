2942bb57864efefe90caf4bb8f7d60810793c686
William Powelson <willpowelson@gmail.com>
2016-12-29 14:31:58 -0800

    fixed eslint errors

--- server/user-db/models/User-Photos-spec.js
+++ server/user-db/models/User-Photos-spec.js
@@ -7,14 +7,14 @@ var Photo = require('./User-Photos.js');
 var expect = require('chai').expect;
 
 var clearDB = (done => {
-    db.sync({force:true})
-      .then(table => {
-        done()
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+  db.sync({force: true})
+    .then(table => {
+      done();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 
 describe('Photos table', () => {
@@ -24,15 +24,15 @@ describe('Photos table', () => {
           {name: 'joe'},
           {name: 'bob'},
           {name: 'gimbo'}
-        ];
+      ];
       User.bulkCreate(users).then(users => {
         done();
-      })
-    })
+      });
+    });
   });
 
   it('Should store photo links to the database', done => {
-    User.find({where:{name:'joe'}})
+    User.find({where: {name: 'joe'}})
       .then(user => {
         return Photo.create({userId: user.id, link: '../../photos-db/joe/hi.jpg'});
       })
@@ -43,6 +43,23 @@ describe('Photos table', () => {
         expect(photo).to.not.equal(null);
         expect(photo.link).to.equal('../../photos-db/joe/hi.jpg');
         done();
+      });
+  });
+
+  it('Should not store links from deleted user', done => {
+    User.find({where: {name: 'joe'}})
+      .then(user => {
+        return Photo.create({userId: user.id, link: '../../photos-db/joe/hi.jpg'});
       })
-  })
+      .then(photo => {
+        return User.destroy({where: {id: photo.userId}});
+      })
+      .then(user => {
+        return Photo.find({where: {userId: user.id}});
+      })
+      .then(photo => {
+        expect(photo).to.equal(null);
+        done();
+      });
+  });
 });
