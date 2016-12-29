a18957c49eb22cd68696d8239d17389307fcd8d1
William Powelson <willpowelson@gmail.com>
2016-12-29 13:23:16 -0800

    Base user model created

--- server/user-db/models/User.js
+++ server/user-db/models/User.js
@@ -1,7 +1,11 @@
 var db = require('./User-db.js');
 var Sequelize = require('sequelize');
 
-
+/*
+The main user model for Fuse usernames are unique
+Note that relations to other tables are defined
+alongisde those tables and are added as decorators
+*/
 var User = db.define('user', {
   name: {
     type: Sequelize.STRING,
