67ad0943a2d8636748015c91b9fb81fa001c6783
William Powelson <willpowelson@gmail.com>
2016-12-29 14:27:48 -0800

    fixed eslint errors

--- server/user-db/models/User-spec.js
+++ server/user-db/models/User-spec.js
@@ -6,42 +6,42 @@ var User = require('./User.js');
 var expect = require('chai').expect;
 
 var clearDB = (done => {
-    db.sync({force:true})
-      .then(table => {
-        done()
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+  db.sync({force: true})
+    .then(table => {
+      done();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 
 describe('User model', () => {
   beforeEach(done => {
-    clearDB(done)
+    clearDB(done);
   });
 
   it('Should be add users to the database', done => {
-    User.create({name:'joe'})
+    User.create({name: 'joe'})
       .then(user => {
         expect(user.name).to.equal('joe');
         done();
       }).catch(err => {
         console.log(err);
-      })
-  })
+      });
+  });
 
   it('Should not allow duplicates of users', () => {
     User.create({name: 'bob'})
       .then(user => {
-        return User.create({name: 'bob'})
+        return User.create({name: 'bob'});
       })
       .then(user => {
-        return User.findAll({where:{name:'bob'}})
+        return User.findAll({where: {name: 'bob'}});
       })
       .then(users => {
         expect(users.length).to.equal(1);
         done();
-      })
-  })
+      });
+  });
 });
