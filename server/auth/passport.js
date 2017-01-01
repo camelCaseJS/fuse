29441cc7c1484094b016fb05775a9aad83624aee
Justin Potter <juspotter@gmail.com>
2016-12-31 16:44:58 -0800

    Basic facebook auth functionality working

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -1,27 +1,25 @@
 
 import passport from 'passport';
-import facebookPassport from 'passport-facebook';
+// Strategy applies to facebook, if other strategies are used need refactor
+import { Strategy } from 'passport-facebook';
 import apiKeys from './oauth-keys';
-import Users from './user';
-
-const FacebookStrategy = facebookPassport.Strategy;
 
 /* Determine what data from the user object to
  * store in the session, in this case req.session.passport.user */
 passport.serializeUser((user, done) => {
+  console.log(`serializeUser ${user.id}`);
   done(null, user.id);
 });
 
-/* Lookup user object based on the key provided to serialize useruse
+/* Lookup user object based on the key provided to serialize user use
  * entire user object is assigned to req.user */
-
 passport.deserializeUser((id, done) => {
-  Users.findByID(id, (user) => {
-    done(null, user);
-  });
+  console.log(`deserializeUser ${id}`);
+  // TODO: use id to read user object from a database, pass to done
+  done(null, id);
 });
 
-passport.use(new FacebookStrategy({
+passport.use(new Strategy({
 
   // Pull facebook API info from oauth-keys.js
   clientID: apiKeys.facebook.clientId,
@@ -30,22 +28,14 @@ passport.use(new FacebookStrategy({
 
 // Facebook will send back the token and profile
 }, (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
   // Find the user based on profile.id
-  Users.findByID(profile.id, (user) => {
-    // If user is found, log them in and return user
-    if (user) {
-      return done(null, user);
-    }
-    // If user not found, create that user
-    const newUser = {
-      id: profile.id,
-      token: accessToken,
-      name: profile.name,
-      email: profile.emails[0].value };
-
-    Users.push(newUser);
-
-    // Log user in and return newUsers
-    return done(null, newUser);
-  });
+    // If user is found
+      // Log them in and return user
+    // If user not found
+      // Create that user
+      // Log user in and return newUsers
+  return done(null, profile);
 }));
+
+export default passport;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,39 @@
 import express from 'express';
+import expressSession from 'express-session';
+import bodyParser from 'body-parser';
+import passport from './auth/passport';
 
 const PORT = 8000;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+  console.log('get request at /');
+  res.send('index');
+});
+
+app.get('/profile', (req, res) => {
+  console.log('get request at /profile');
+  res.send('profile');
+});
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook', { scope: 'email' }), (req, res) => {
+    console.log(`auth success with ${req.user}`);
+  });
+
+// app.get('/auth/facebook/callback', (req, res) => {
+//   res.send('callback');
+// });
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+    failureRedirect: '/profile' }));
 
 app.listen(PORT);
 
