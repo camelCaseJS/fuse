9b70ae252b2a9a44618792309614be01258ab09c
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2017-01-02 16:08:14 -0800

    Start user-router, Create placeholder files in client.

--- server/user/user-router.js
+++ server/user/user-router.js
@@ -1,5 +1,23 @@
 import express from 'express';
+import Friendship from '../db/users/User-Friends';
 
 const app = express();
+
+app.get('/users', (req, res) => {
+  Friendship.findAll({
+    where: {
+      userId: req.body,
+    },
+  })
+  .then((friends) => {
+    res.json(friends);
+  });
+  // res.send();
+});
+
+
+app.post('/users', (req, res) => {
+  res.send();
+});
 
 export default app;
