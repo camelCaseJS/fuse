58e3794bf7610e88cdf61afeab27c47d354f72de
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2017-01-03 15:14:38 -0800

    Create redirects for missing session, or missing friends

--- server/user/user-router.js
+++ server/user/user-router.js
@@ -5,8 +5,9 @@ const User = require('../db/users/User');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log(req.session);
+  // console.log(req.session);
   if (req.session.passport) {
+    console.log(req.session.passport.user);
     User.findOne({
       where: {
         id: req.session.passport.user,
@@ -30,13 +31,31 @@ app.get('/', (req, res) => {
 
 // post req should take in id of friend the user wants to add. :id in route is friend's id
 
+app.post('/', (req, res) => {
+  console.error('select a friend to add');
+  res.redirect('/');
+});
+
 app.post('/:id', (req, res) => {
-  Friendship.create({
-    userId: req.session.passport.user,
-    friendId: req.params.id,
+  Friendship.findOne({
+    where: {
+      id: req.params.id,
+    },
   })
-  .then((id) => {
-    res.send(id);
+  .then((friend) => {
+    if (friend.length !== 0) {
+      Friendship.create({
+        userId: req.session.passport.user,
+        friendId: req.params.id,
+      })
+      .then((id) => {
+        console.log('friend created');
+        res.send(id);
+      });
+    } else {
+      console.error('Friend not found');
+      res.redirect('/');
+    }
   });
 });
 
