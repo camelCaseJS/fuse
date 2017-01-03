fa223d8b37e4f5a4881bb425155ed77e88cc51d8
Justin Potter <juspotter@gmail.com>
2017-01-03 14:43:16 -0800

    Updated User-Photos-Spec for new user db schema

--- server/db/users/User-Photos-spec.js
+++ server/db/users/User-Photos-spec.js
@@ -21,9 +21,12 @@ describe('Photos table', () => {
   beforeEach((done) => {
     clearDB(() => {
       const users = [
-          { name: 'joe' },
-          { name: 'bob' },
-          { name: 'gimbo' },
+        { facebookId: '123',
+          email: '123@gmail.com' },
+        { facebookId: '456',
+          email: '456@gmail.com' },
+        { facebookId: '789',
+          email: '789@gmail.com' },
       ];
       User.bulkCreate(users).then(() => {
         done();
@@ -32,12 +35,12 @@ describe('Photos table', () => {
   });
 
   it('Should store photo links to the database', (done) => {
-    User.find({ where: { name: 'joe' } })
-      .then(user => Photo.create({ userId: user.id, link: '../../photos-db/joe/hi.jpg' }))
+    User.find({ where: { facebookId: '123' } })
+      .then(user => Photo.create({ userId: user.id, link: '../../photos-db/123/hi.jpg' }))
       .then(photo => Photo.find({ where: { id: photo.id } }))
       .then((photo) => {
         expect(photo).to.not.equal(null);
-        expect(photo.link).to.equal('../../photos-db/joe/hi.jpg');
+        expect(photo.link).to.equal('../../photos-db/123/hi.jpg');
         done();
       })
       .catch((err) => {
@@ -48,8 +51,8 @@ describe('Photos table', () => {
   });
 
   it('Should not store links from deleted user', (done) => {
-    User.find({ where: { name: 'joe'} })
-      .then(user => Photo.create({ userId: user.id, link: '../../photos-db/joe/hi.jpg' }))
+    User.find({ where: { facebookId: '123'} })
+      .then(user => Photo.create({ userId: user.id, link: '../../photos-db/123/hi.jpg' }))
       .then(photo => User.destroy({ where: { id: photo.userId } }))
       .then(user => Photo.find({ where: { userId: user.id } }))
       .then((photo) => {
@@ -65,9 +68,9 @@ describe('Photos table', () => {
 
   it('Should not store duplicate links', (done) => {
     const link = '../../photos-db/mint/hi.jpg';
-    User.find({ where: { name: 'bob' } })
+    User.find({ where: { facebookId: '456' } })
     .then(user => Photo.create({ userId: user.id, link }))
-    .then(() => User.find({ where: { name: 'joe' } }))
+    .then(() => User.find({ where: { facebookId: '123' } }))
     .then(user => Photo.create({ userId: user.id, link }))
     .then((photo) => {
       console.log('successfully created duplicate');
