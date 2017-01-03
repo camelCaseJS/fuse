cd7049153e4ba9c4aa883788765eea2d16bb9708
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2017-01-02 20:25:20 -0800

    Create user routes

--- server/user/user-router.js
+++ server/user/user-router.js
@@ -1,23 +1,38 @@
-import express from 'express';
-import Friendship from '../db/users/User-Friends';
+const express = require('express');
+const Friendship = require('../db/users/User-Friends');
+const User = require('../db/users/User');
 
 const app = express();
 
 app.get('/users', (req, res) => {
-  Friendship.findAll({
+  User.findOne({
     where: {
-      userId: req.body,
-    },
-  })
-  .then((friends) => {
-    res.json(friends);
-  });
+      facebookId: req.session.passport.user,
+    }
+    .then((userId) => {
+      Friendship.findAll({
+        where: {
+          id: userId,
+        },
+      })
+      .then((friends) => {
+        res.json(friends);
+      });
+    }),
   // res.send();
+  });
 });
 
+// post req should take in id of friend the user wants to add. :id in route is friend's id
 
-app.post('/users', (req, res) => {
-  res.send();
+app.post('/users/:id', (req, res) => {
+  Friendship.create({
+    userId: req.session.passport.user,
+    friendId: req.params.id,
+  })
+  .then((id) => {
+    res.send(id);
+  });
 });
 
-export default app;
+module.exports = app;
