f83d5d5041158a5010a4b07086e57d5a907b58cb
Justin Potter <juspotter@gmail.com>
2017-01-03 13:25:32 -0800

    Added facebookID, firstName, lastName, email and pofilePictureURL to User table

--- server/db/users/User.js
+++ server/db/users/User.js
@@ -7,11 +7,19 @@ Note that relations to other tables are defined
 alongisde those tables and are added as decorators
 */
 const User = db.define('user', {
-  name: {
+  facebookId: {
     type: Sequelize.STRING,
     allowNull: false,
     unique: true,
   },
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  profilePictureURL: Sequelize.STRING,
 });
 
 module.exports = User;
