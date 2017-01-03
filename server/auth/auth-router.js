5e8b4785b43d109177bfc88547ecb79b31f947d8
Justin Potter <juspotter@gmail.com>
2017-01-02 18:19:16 -0800

    Added /logout route to auth router

--- server/auth/auth-router.js
+++ server/auth/auth-router.js
@@ -6,6 +6,15 @@ const app = express();
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect('/');
+  });
+});
+
 app.get('/facebook',
   passport.authenticate('facebook', { scope: 'email' }), (req, res) => {
   });
