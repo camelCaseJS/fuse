6b02a58bd70bfa59de752ae22279c9eec967a08e
William Powelson <willpowelson@gmail.com>
2017-01-03 16:38:35 -0800

    remove unneeded dependencies

--- server/photo/photo-handler.js
+++ server/photo/photo-handler.js
@@ -1,51 +1,52 @@
-const User = require('../db/users/User.js')
-const Friends = require('../db/users/User-Friends.js')
-const Photos = require('../db/users/User-Photos.js')
 const fs = require('fs');
 const path = require('path');
+
 const storageDir = path.resolve(__dirname, '../db/photos');
 
-const user = 'bobby';
 /*
-* 
+* saves photo a in a folder, creates the folder if it does not exist
 *
+* inputs: multi-form data request with a user identifier and file attached
+* output: success or failure code response
 *
+* NOTE: checking for folder existence first will cause race conditions,
+* refactors should not implement such functionality
 */
 module.exports.addPhoto = (req, res) => {
-  if(!req.body.user || !req.files.image) {
+  if (!req.body.user || !req.files.image) {
     res.sendStatus(403);
   } else {
     const userDir = path.resolve(storageDir, req.body.user);
-    const savePhoto = function() {
-      fs.writeFile(userDir + '/' + req.files.image.name, req.files.image.data, (err) => {
+    const savePhoto = () => {
+      const fileLoc = path.resolve(path.userDir, '/', req.files.image.name);
+      fs.writeFile(fileLoc, req.files.image.data, (err) => {
         if (err) {
           console.log(err);
-          res.sendStatus(403)
+          res.sendStatus(403);
         } else {
-         res.sendStatus(201);
+          res.sendStatus(201);
         }
       });
-    }
-    fs.mkdir(userDir, null, function(err) {
+    };
+    fs.mkdir(userDir, null, (err) => {
       if (err) {
-        if (err.code == 'EEXIST') {
+        if (err.code === 'EEXIST') {
           savePhoto(); // ignore the error if the folder already exists
         } else {
           res.sendStatus(403);
         } // something else went wrong
       } else {
-        savePhoto()
-      }; // successfully created folder
+        savePhoto();
+      } // successfully created folder
     });
   }
 };
 
 module.exports.getPhotos = (req, res) => {
-  const friends = req.body.friends;
-  const
+  res.send('boop');
 };
 
 
 module.exports.deletePhoto = (req, res) => {
-  res.send('goodbye cruel world')
+  res.send('goodbye cruel world');
 };
