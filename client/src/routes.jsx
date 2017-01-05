ea4ca13b620671d2d228293f0e933a6467974f76
Justin Potter <juspotter@gmail.com>
2017-01-04 21:15:11 -0800

    Converted router naming to routes for photos, friends and camera

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,15 +2,15 @@
 import React from 'react';
 import { Route } from 'react-router';
 
-import PhotosRouter from './photos-view/photosRouter';
-import FriendsRouter from './friends-view/friendsRouter';
-import CameraRouter from './camera-view/cameraRouter';
+import PhotosRoutes from './photos-view/photosRoutes';
+import FriendsRoutes from './friends-view/friendsRoutes';
+import CameraRoutes from './camera-view/cameraRoutes';
 import App from './app';
 
 export default (
   <Route path="/" component={App}>
-    <Route path="photos" component={PhotosRouter} />
-    <Route path="friends" component={FriendsRouter} />
-    <Route path="camera" component={CameraRouter} />
+    <Route path="photos" component={PhotosRoutes} />
+    <Route path="friends" component={FriendsRoutes} />
+    <Route path="camera" component={CameraRoutes} />
   </Route>
 );
