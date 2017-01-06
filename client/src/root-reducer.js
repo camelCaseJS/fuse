6fd0293688dd9d028bf5b5dd6f91c190a712598d
Justin Potter <juspotter@gmail.com>
2017-01-06 12:42:02 -0800

    Added all reducers to combineReducers

--- client/src/root-reducer.js
+++ client/src/root-reducer.js
@@ -7,12 +7,7 @@ import photos from './photos/reducers/reducers';
 import friends from './friends/reducers/reducers';
 import camera from './camera/reducers/reducers';
 
-// const defaultReducer = (state = null, action) => {
-//   return state;
-// };
-
-// const allReducers = Object.assign({}, photosReducers, friendsReducers, cameraReducers);
-const rootReducer = combineReducers({ friends: friends });
+const rootReducer = combineReducers({ friends, camera, photos });
 
 export default rootReducer;
 
