0cf3cad6d49b706111a0445293d1ad63cfe4b253
Justin Potter <juspotter@gmail.com>
2017-01-07 12:28:22 -0800

    Improved routes with proper auth

--- server/server.js
+++ server/server.js
@@ -7,15 +7,32 @@ const isAuthenticated = require('./auth/is-authenticated');
 
 const PORT = 8000;
 
+// API routes
 app.use('/auth', authRouter);
 app.use('/photo', isAuthenticated, photoRouter);
 app.use('/user', isAuthenticated, userRouter);
 
+// Serve up webpack bundle
 app.get('/dist/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'bundle.js'));
 });
 
-app.get(/(\/$|^\/#)/, (req, res) => {
+// Requests to index will be authenticated
+// If authenitcated, user sent to landing page, /friends
+// If not authenitcated, controller routes to login
+app.get('/', isAuthenticated, (req, res) => {
+  res.redirect('/friends');
+});
+
+// Login path doesn't require authentication
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
+
+// React router paths directed to index.html
+const reactRouterPaths = ['/friends', '/photos', '/camera'];
+
+app.get(reactRouterPaths, isAuthenticated, (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
 
