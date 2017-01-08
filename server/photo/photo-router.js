d7f4a74f9571b0bdf563802d9df29182c63ee886
William Powelson <willpowelson@gmail.com>
2017-01-07 17:04:39 -0800

    file upload middleware

--- server/photo/photo-router.js
+++ server/photo/photo-router.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const photoRouter = express.Router();
 const photoHandler = require('./photo-handler');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 
-photoRouter.use(fileUpload());
+const photoRouter = express.Router();
 
-photoRouter.use((req, res, next) => {
-  req.user = {};
-  req.user.id = 1;
-  next()
-})
+photoRouter.use(fileUpload());
 
 photoRouter.post('/', photoHandler.addPhoto);
 
@@ -19,7 +14,7 @@ photoRouter.use(bodyParser.json());
 
 photoRouter.get('/', photoHandler.getAllLinks);
 
-photoRouter.get('/:user', photoHandler.getUserLinks)
+photoRouter.get('/:user', photoHandler.getUserLinks);
 
 photoRouter.get('/:user/:photo', photoHandler.getPhoto);
 
