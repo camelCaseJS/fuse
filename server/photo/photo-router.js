0c730b1087a6a174cc072752cea4c8bdd19a102c
William Powelson <willpowelson@gmail.com>
2017-01-07 16:19:30 -0800

    adds route for individual user links

--- server/photo/photo-router.js
+++ server/photo/photo-router.js
@@ -2,22 +2,27 @@ const express = require('express');
 const photoRouter = express.Router();
 const photoHandler = require('./photo-handler');
 const bodyParser = require('body-parser')
-const path = require('path')
-const multipart = require('connect-multiparty');
 const fileUpload = require('express-fileupload');
-const multipartMiddleware = multipart();
-const uploads = path.resolve(__dirname, '../db/photos');
 
 photoRouter.use(fileUpload());
-//needs raw body data
+
+photoRouter.use((req, res, next) => {
+  req.user = {};
+  req.user.id = 1;
+  next()
+})
+
 photoRouter.post('/', photoHandler.addPhoto);
 
 photoRouter.use(bodyParser.urlencoded({ extended: true }));
 photoRouter.use(bodyParser.json());
 
-photoRouter.get('/:user', photoHandler.getPhotos);
+photoRouter.get('/', photoHandler.getAllLinks);
 
+photoRouter.get('/:user', photoHandler.getUserLinks)
 
-photoRouter.delete('/', photoHandler.deletePhoto);
+photoRouter.get('/:user/:photo', photoHandler.getPhoto);
+
+photoRouter.delete('/:user/:photo', photoHandler.deletePhoto);
 
 module.exports = photoRouter;
