02b453ff5fabc4b37e265826e8bf99adb4f22b26
Justin Potter <juspotter@gmail.com>
2017-01-14 15:00:56 -0800

    attempt to build in xcode

--- mobile/app/containers/root-container.jsx
+++ mobile/app/containers/root-container.jsx
@@ -1,35 +1,25 @@
-// @flow
-
-import React, { Component } from 'react'
-import { View, StatusBar } from 'react-native'
-import NavigationRouter from '../Navigation/NavigationRouter'
-import { connect } from 'react-redux'
-import StartupActions from '../Redux/StartupRedux'
+import React, { Component } from 'react';
+import { View, StatusBar } from 'react-native';
+import { connect } from 'react-redux';
+import NavigationRouter from '../navigation/navigation-router';
 
 // Styles
-import styles from './Styles/RootContainerStyle'
+import styles from './styles/RootContainerStyle';
 
 class RootContainer extends Component {
-  componentDidMount () {
-    // if redux persist is not active fire startup action
-    if (!ReduxPersist.active) {
-      this.props.startup()
-    }
-  }
-
-  render () {
+  render() {
     return (
       <View style={styles.applicationView}>
-        <StatusBar barStyle='light-content' />
+        <StatusBar barStyle="light-content" />
         <NavigationRouter />
       </View>
-    )
+    );
   }
 }
 
 // wraps dispatch to create nicer functions to call within our component
-const mapDispatchToProps = (dispatch) => ({
-  startup: () => dispatch(StartupActions.startup())
-})
+const mapDispatchToProps = dispatch => ({
+  startup: () => dispatch(),
+});
 
-export default connect(null, mapDispatchToProps)(RootContainer)
+export default connect(null, mapDispatchToProps)(RootContainer);
---
+++ mobile/app/navigation/nav-items.jsx
@@ -0,0 +1,60 @@
+// @flow
+
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Actions as NavigationActions } from 'react-native-router-flux';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from './styles/NavItemsStyle';
+import { Colors, Metrics } from '../themes';
+
+const openDrawer = () => {
+  NavigationActions.refresh({
+    key: 'drawer',
+    open: true,
+  });
+};
+
+const navItem = {
+  backButton() {
+    return (
+      <TouchableOpacity onPress={NavigationActions.pop}>
+        <Icon
+          name="angle-left"
+          size={Metrics.icons.large}
+          color={Colors.snow}
+          style={styles.backButton}
+        />
+      </TouchableOpacity>
+    );
+  },
+
+  hamburgerButton() {
+    return (
+      <TouchableOpacity onPress={openDrawer}>
+        <Icon
+          name="bars"
+          size={Metrics.icons.medium}
+          color={Colors.snow}
+          style={styles.navButtonLeft}
+        />
+      </TouchableOpacity>
+    );
+  },
+
+  searchButton(callback) {
+    return (
+      <TouchableOpacity onPress={callback}>
+        <Icon
+          name="search"
+          size={Metrics.icons.small}
+          color={Colors.snow}
+          style={styles.searchButton}
+        />
+      </TouchableOpacity>
+    );
+  },
+
+};
+
+export default navItem;
+
