1c32fa4507351bfe6eb4aabccdfce860cd179ca8
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2017-01-16 20:46:05 -0800

    create placeholder div for testing

--- client/src/features/photos/containers/photos.jsx
+++ client/src/features/photos/containers/photos.jsx
@@ -4,14 +4,16 @@ import Friends from '../../friends/containers/friends';
 import PhotosList from '../components/photos-list';
 
 
-const Photos = () => {
-
-  return (
-    <Main
-      left={<Friends />}
-      right={<PhotosList />}
-    />
-  );
-};
+const Photos = () => (
+  (<Main
+    left={<Friends />}
+    right={
+      <img
+        src="https://cdn.meme.am/cache/images/folder459/300x/8557459.jpg"
+        role="presentation"
+      />
+    }
+  />)
+);
 
 export default Photos;
