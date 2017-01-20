4bb0acc5762dbd3e71c4cca2f3a88b3b96b13f7f
Justin Potter <juspotter@gmail.com>
2017-01-19 23:19:14 -0800

    Remove test user from env variables

--- server/middleware/process-env-variables.js
+++ server/middleware/process-env-variables.js
@@ -8,12 +8,12 @@ const buildProcessEnv = () => {
   // process.env.DB_PORT = 5433;
   process.env.NODE_ENV = 'development';
   // process.env.TEST_USER = {
-//       facebookId: '12345',
-//       firstName: 'first',
-//       lastName: 'last',
-//       email: 'test@fuse.com',
-//       profilePictureURL: 'https://image.freepik.com/free-icon/question-mark_318-52837.jpg',
-//     }
+  //     facebookId: '12345',
+  //     firstName: 'first',
+  //     lastName: 'last',
+  //     email: 'test@fuse.com',
+  //     profilePictureURL: 'https://image.freepik.com/free-icon/question-mark_318-52837.jpg',
+  //   }
 };
 
 module.exports = buildProcessEnv;
